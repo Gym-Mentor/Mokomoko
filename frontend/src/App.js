cf19ba72a6dad2b2192cecb8e3293fb2dff97cd1
edenist-x-x <yaron_e5@naver.com>
2021-07-26 15:20:39 +0900

    🔨 **develop** : 회원가입, 로그인 페이지 구현

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,22 @@
 import { Route, Switch } from "react-router-dom";
 import "./App.css";
 import "./css/common.css";
-import Login from "./pages/Login";
-import Forgot from "./components/Forgot";
-import Join from "./pages/Join";
+import Login from "./pages/user/Login";
+import Forgot from "./components/user/Forgot";
+import Join from "./pages/user/Join";
 import Error404 from "./components/errors/Error404";
-import NaverTest from "./pages/NaverTest";
-import Test from "./pages/Test";
+import NaverTest from "./pages/user/NaverTest";
+import Test from "./pages/user/Test";
 
 function App() {
   return (
     <div className="App">
       <Switch>
-        <Route path="/login" component={Login} exact />
-        <Route path="/forgot" component={Forgot} />
-        <Route path="/join" component={Join} />
-        <Route path="/naverLogin" component={NaverTest} />
-        <Route path="/kakaoLogin" component={Test} />
+        <Route path="/account/login" component={Login} exact />
+        <Route path="/account/forgot" component={Forgot} />
+        <Route path="/account/join" component={Join} />
+        <Route path="/account/naverLogin" component={NaverTest} />
+        <Route path="/account/kakaoLogin" component={Test} />
 
         <Route component={Error404} />
       </Switch>
