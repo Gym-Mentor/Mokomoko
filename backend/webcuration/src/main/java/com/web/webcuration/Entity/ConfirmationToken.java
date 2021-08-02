694b16acc98ba3e43915110a8c26a1793d62861e
김범수 <qhrud0527@naver.com>
2021-08-02 13:58:20 +0900

    🔨 develop : 회원 인증 이메일 발송

--- backend/webcuration/src/main/java/com/web/webcuration/Entity/ConfirmationToken.java
+++ backend/webcuration/src/main/java/com/web/webcuration/Entity/ConfirmationToken.java
@@ -8,9 +8,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-import org.hibernate.annotations.GenericGenerator;
 import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
