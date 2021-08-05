a87330989ab44897a86196bd6946760fa68b383a
박종한 <jonghan1983@naver.com>
2021-08-05 14:25:36 +0900

    🔥 **remove** : NaverTest(콜백 페이지) 삭제 및 리팩토링

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,6 @@ import "./css/common.css";
 import Login from "./components/user/Login";
 import Join from "./components/user/Join";
 import Error404 from "./components/errors/Error404";
-import NaverTest from "./components/user/NaverTest";
 import Test from "./components/user/Test";
 import JoinConfirm from "./components/user/JoinConfirm";
 import Feed from "./components/main/Feed";
@@ -24,7 +23,6 @@ function App() {
       <Switch>
         <Route path="/account/login" component={Login} exact />
         <Route path="/account/join" component={Join} />
-        <Route path="/account/naverLogin" component={NaverTest} />
         <Route path="/account/kakaoLogin" component={Test} />
         <Route path="/account/joinConfirm" component={JoinConfirm} />
 
@@ -34,7 +32,7 @@ function App() {
         <Route path="/main/settingNick" component={NicknameSetting} />
         <Route path="/main/compass" component={Compass} />
         <Route path="/main/profile" component={Profile} />
-        <Route path="/main/detail/:index" component={Detail}/>
+        <Route path="/main/detail/:index" component={Detail} />
         <Route path="/main/select" component={Select} />
         <Route path="/main/writeFood" component={Food} />
         <Route path="/main/writeFoodText" component={FoodText} />
