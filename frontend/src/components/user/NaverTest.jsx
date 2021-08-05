05dd8487d7cf634349dbd690b1df28eb98c52c3d
박종한 <jonghan1983@naver.com>
2021-08-05 14:03:39 +0900

    🔨 **develop** : 네이버 로그인 백엔드와 통신 update

--- frontend/src/components/user/NaverTest.jsx
+++ frontend/src/components/user/NaverTest.jsx
@@ -1,8 +1,8 @@
 import React, { useEffect } from "react";
-
+import axios from "axios";
 const { naver } = window;
 const location = window.location;
-const NaverTest = () => {
+const NaverTest = (props) => {
   const naverLogin = new naver.LoginWithNaverId({
     clientId: "aZzX9db7PmSlUCampLBA",
     callbackUrl: "http://localhost:3000/account/naverLogin",
@@ -17,15 +17,32 @@ const NaverTest = () => {
 
     naverLogin.getLoginStatus(function (status) {
       if (status) {
+        //auth/sns post방식
         /* (5) 필수적으로 받아야하는 프로필 정보가 있다면 callback처리 시점에 체크 */
-        var email = naverLogin.user.getEmail();
-        var id = naverLogin.user.getId();
-        var name = naverLogin.user.getName();
-        console.log(email);
-        console.log(id);
-        console.log(name);
-        console.log(naverLogin.user);
+        let image = naverLogin.user.getProfileImage();
+        let id = naverLogin.user.getId();
+        let nickname = naverLogin.user.getNickName();
+        const data = {
+          id: id,
+          nickname: nickname,
+          image: image,
+        };
         // axios로 백엔드와 통신 후 login 페이지로 redirect
+
+        axios({
+          method: "post",
+          url: "/auth/sns",
+          data: data,
+        })
+          .then((res) => {
+            console.log(res);
+            props.history.push("/main/feed");
+          })
+          .catch((error) => {
+            console.error(error);
+            props.history.push("/account/login");
+            // alert("네이버 로그인에 실패했습니다.");
+          });
       }
     });
   }, []);
