4e18f21e447e275a89b43806365b63be3fe1326d
박종한 <jonghan1983@naver.com>
2021-08-06 11:02:04 +0900

    🔨 **develop** : 네이버 아이디로 로그인 update

--- frontend/src/components/user/NaverCallBack.jsx
+++ frontend/src/components/user/NaverCallBack.jsx
@@ -3,13 +3,15 @@ import axios from "axios";
 const { naver } = window;
 const NaverCallBack = (props) => {
   const naverLogin = new naver.LoginWithNaverId({
-    clientId: "aZzX9db7PmSlUCampLBA",
+    clientId: "nwk_DGz4Rg8qXKe4QUws",
     callbackUrl: "http://localhost:3000/account/naverLogin",
     isPopup: false, // popup 형식으로 띄울것인지 설정
     callbackHandle: true,
   });
+  naverLogin.init();
   useEffect(() => {
     naverLogin.getLoginStatus(function (status) {
+      console.log(status);
       if (status) {
         let image = naverLogin.user.getProfileImage();
         let id = naverLogin.user.getId();
@@ -19,6 +21,7 @@ const NaverCallBack = (props) => {
           nickname: nickname,
           image: image,
         };
+        console.log(data);
         // axios로 백엔드와 통신 후 login 페이지로 redirect
         props.history.push("/account/login");
         axios({
