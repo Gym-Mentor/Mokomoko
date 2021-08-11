0743de29711e38f6c3e3fb88fb0a95cf99c758cd
김범수 <qhrud0527@naver.com>
2021-08-11 16:53:23 +0900

    🔧 build : PostController 수정

--- backend/webcuration/src/main/java/com/web/webcuration/controller/PostsController.java
+++ backend/webcuration/src/main/java/com/web/webcuration/controller/PostsController.java
@@ -2,6 +2,9 @@ package com.web.webcuration.controller;
 
 import java.util.HashMap;
 
+import javax.mail.Multipart;
+
+import com.web.webcuration.dto.ContentDto;
 import com.web.webcuration.dto.response.BaseResponse;
 import com.web.webcuration.service.PostsService;
 
@@ -10,6 +13,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import lombok.RequiredArgsConstructor;
 
@@ -21,9 +25,12 @@ public class PostsController {
     private final PostsService postsService;
 
     @PostMapping("/")
-    public ResponseEntity<BaseResponse> createPosts(@RequestBody Object newPosts) {
+    public ResponseEntity<BaseResponse> createPosts(MultipartFile multipartFiles, String key) {
         // postsService.createPosts(newPosts);
+        ContentDto content = new ContentDto();
+        content.setDesc(key);
+        content.setImages(multipartFiles);
 
-        return ResponseEntity.ok(new BaseResponse("200", "success", newPosts));
+        return ResponseEntity.ok(new BaseResponse("200", "success", content));
     }
 }
