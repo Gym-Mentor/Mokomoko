006c79923e408a9c014c664125f3975fbdd95c25
박종한 <jonghan1983@naver.com>
2021-08-14 19:57:36 +0900

    🐛 **fix** : 서버 이미지 url 확인용

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -17,18 +17,18 @@ function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <Switch>
-          <Route path="/" component={Test} exact />
-          <Route path="/account/login" component={Login} />
-          <Route path="/account/join" component={Join} />
-          <Route path="/account/naverLogin" component={NaverCallBack} />
-          <Route path="/account/joinConfirm" component={JoinConfirm} />
-          <Route path="/account/forgot" component={ForgotPassword} />
-          <Route path="/account/enterCode/:email" component={EnterCode} />
-          <Route path="/account/updatepw/:email" component={UpdatePW} />
-          <Route path="/main" component={Main} />
-          <Route component={Error404} />
-        </Switch>
+        {/* <Switch> */}
+        <Route path="/" component={Test} exact />
+        <Route path="/account/login" component={Login} />
+        <Route path="/account/join" component={Join} />
+        <Route path="/account/naverLogin" component={NaverCallBack} />
+        <Route path="/account/joinConfirm" component={JoinConfirm} />
+        <Route path="/account/forgot" component={ForgotPassword} />
+        <Route path="/account/enterCode/:email" component={EnterCode} />
+        <Route path="/account/updatepw/:email" component={UpdatePW} />
+        <Route path="/main" component={Main} />
+        {/* <Route component={Error404} /> */}
+        {/* </Switch> */}
         {/* <Route path="/" component={Test} exact="exact"/> */}
       </BrowserRouter>
     </div>
