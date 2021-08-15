cdebe662581825a8ba74a4ccb6e9c56b80f633e3
김범수 <qhrud0527@naver.com>
2021-08-15 22:33:37 +0900

    🔧 build : 닉네임 수정 완료

---
+++ backend/webcuration/src/main/java/com/web/webcuration/dto/request/NickNameRequest.java
@@ -0,0 +1,13 @@
+package com.web.webcuration.dto.request;
+
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@ToString
+public class NickNameRequest {
+
+    private Long id;
+
+    private String nickname;
+}
