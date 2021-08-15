f7a14f6563b27275621d47e011e8d826c803e77c
김범수 <qhrud0527@naver.com>
2021-08-15 17:24:16 +0900

    🔨 **develop** : 파일 업로드 기능 추가

--- backend/webcuration/src/main/java/com/web/webcuration/utils/FileUtils.java
+++ backend/webcuration/src/main/java/com/web/webcuration/utils/FileUtils.java
@@ -11,35 +11,72 @@ import javax.swing.filechooser.FileSystemView;
 import com.web.webcuration.Entity.Contents;
 import com.web.webcuration.dto.ContentDto;
 
+import org.springframework.web.multipart.MultipartFile;
+
 public class FileUtils {
 
-    public static List<Contents> fileUpload(List<ContentDto> reqContents, Long postid)
+    public static List<Contents> uploadFile(List<ContentDto> reqContents, Long postid)
             throws IllegalStateException, IOException {
         List<Contents> resContents = new ArrayList<>();
         String rootPath = FileSystemView.getFileSystemView().getHomeDirectory().toString();
         // local환경
-        // String basePath = rootPath + "/" + "img";
+        String basePath = rootPath + "/" + "img";
         // 서버 환경
-        String basePath = "/home/img";
+        // String basePath = "/home/img";
         Integer index = 1;
         for (ContentDto content : reqContents) {
             UUID uuid = UUID.randomUUID();
-            String contentType = content.getMedia().getContentType();
-            String originalFileExtension;
-            if (contentType.equals("image/jpeg")) {
-                originalFileExtension = ".jpg";
-            } else if (contentType.equals("image/png")) {
-                originalFileExtension = ".png";
-            } else if (contentType.equals("image/gif")) {
-                originalFileExtension = ".gif";
-            } else {
-                throw new RuntimeException("이미지가 아닙니다");
-            }
-            String filePath = basePath + "/" + uuid.toString() + originalFileExtension;
+            String filePath = basePath + "/" + uuid.toString() + getContentType(content.getMedia());
             resContents.add(content.toContents(postid, filePath, index++));
             File dest = new File(filePath);
             content.getMedia().transferTo(dest);
         }
         return resContents;
+    }
+
+    public static void deleteFile(List<Contents> reqContents) {
+        for (Contents content : reqContents) {
+            String filepath = content.getImage();
+            File file = new File(filepath);
+            if (file.exists()) {
+                file.delete();
+            }
+        }
+    }
+
+    public static String uploadProfile(MultipartFile profileImage) throws IllegalStateException, IOException {
+        UUID uuid = UUID.randomUUID();
+        String rootPath = FileSystemView.getFileSystemView().getHomeDirectory().toString();
+        // local환경
+        String basePath = rootPath + "/" + "img";
+        // 서버 환경
+        // String basePath = "/home/img";
+        String filePath = basePath + "/" + uuid.toString() + getContentType(profileImage);
+        File dest = new File(filePath);
+        profileImage.transferTo(dest);
+
+        return filePath;
+    }
+
+    public static void deleteProfile(String profileFilePath) {
+        File file = new File(profileFilePath);
+        if (file.exists()) {
+            file.delete();
+        }
+    }
+
+    public static String getContentType(MultipartFile file) {
+        String contentType = file.getContentType();
+        String originalFileExtension;
+        if (contentType.equals("image/jpeg")) {
+            originalFileExtension = ".jpg";
+        } else if (contentType.equals("image/png")) {
+            originalFileExtension = ".png";
+        } else if (contentType.equals("image/gif")) {
+            originalFileExtension = ".gif";
+        } else {
+            throw new RuntimeException("이미지가 아닙니다");
+        }
+        return originalFileExtension;
     }
 }
