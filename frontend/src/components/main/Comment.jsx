df5b7dc299aed5ade14c5ade3f22e156a6c0bd8c
Dayoung98 <iiilll_iill@naver.com>
2021-08-17 22:33:55 +0900

    🔨 **develop** : 댓글 기능

--- frontend/src/components/main/Comment.jsx
+++ frontend/src/components/main/Comment.jsx
@@ -1,15 +1,62 @@
-import React from "react";
+import React ,{useState,useEffect} from "react";
+import { useSelector, useDispatch } from "react-redux";
 import "../../css/main/Comment.css";
 import ContentHeader from "../header/ContentHeader";
 import { Avatar } from "@material-ui/core";
 import CheckCircleOutlineIcon from "@material-ui/icons/CheckCircleOutline";
 import CheckCircleIcon from "@material-ui/icons/CheckCircle";
 import FavoriteBorderIcon from "@material-ui/icons/FavoriteBorder";
+import axios from "axios";
 
-const Comment = (profilePhoto, nickname, comment) => {
-  const onClickBack = () => {
-    console.log("뒤로가기 처리");
+import { setComments } from "../../modules/Post";
+
+const Comment = () => {
+
+  const {user,userImage,post,comments} = useSelector(state => ({
+    user : state.userInfo.user,
+    userImage : state.Post.userImage,
+    post : state.Post.post,
+    comments : state.Post.comments,
+  }));
+
+  const dispatch = useDispatch();
+  const onSetComments = (comments) => dispatch(setComments(comments));
+  
+  const [writeComment,setWriteComment] = useState(null);
+
+  const onChangeWriteComment = (e) =>{
+    setWriteComment(e.target.value);
   };
+
+  const submitComment = () =>{
+    console.log(writeComment);
+    console.log("댓글 전송");
+
+    axios({
+      method : "post",
+      url: "http://i5d104.p.ssafy.io:8080/comment",
+      data :{
+        "userid": user.id,
+        "postid": post.id,
+        "description": writeComment,
+      }
+    })
+    .then((response) =>{
+      console.log(response);
+    })
+    .catch((error) =>{
+      console.log(error);
+    })
+
+    setWriteComment(null);
+  }
+
+  useEffect(() => {
+    
+    return () => {
+      
+    }
+  }, [writeComment])
   return (
     <div className="comments-wrapper">
       <ContentHeader title="댓글" />
@@ -19,15 +66,17 @@ const Comment = (profilePhoto, nickname, comment) => {
           {/* container1, 2 는 댓글 작성자의 입력폼 */}
           <div className="comment-type-container2">
             <div className="comment-avatar-container">
-              <Avatar id="comment-avatar" className="post-avatar" src={profilePhoto} />
+              <Avatar id="comment-avatar" className="post-avatar" src={userImage} />
             </div>
             <textarea
               type="textarea"
               id="comment-ipt"
               name="commenttext"
               placeholder="댓글 달기..."
+              value={writeComment}
+              onChange={onChangeWriteComment}
             ></textarea>
-            <button type="submit" className="comment-btn">
+            <button type="submit" className="comment-btn" onClick={submitComment}>
               <CheckCircleIcon id="submit-icon" />
             </button>
             {/* </div> */}
@@ -37,9 +86,9 @@ const Comment = (profilePhoto, nickname, comment) => {
 
       <div className="user-comment">
         <ul className="comment-list">
-          <li className="comment-list-detail">
+          {/* <li className="comment-list-detail">
             <div className="usr-comment-userInfo">
-              <Avatar id="usr-comment-avatar" className="post-avatar" src={profilePhoto} />
+              <Avatar id="usr-comment-avatar" className="post-avatar" src={userImage} />
               <p className="usr-comment-username">username</p>
               <span className="usr-comment-desc">
                 this is test comment. plz type right messages
@@ -62,13 +111,15 @@ const Comment = (profilePhoto, nickname, comment) => {
                 </div>
               </div>
             </div>
-          </li>
-          <li className="comment-list-detail">
+          </li> */}
+          {comments && comments.map((item,index) =>{
+            return(
+            <li className="comment-list-detail" key = {index}>
             <div className="usr-comment-userInfo">
-              <Avatar id="usr-comment-avatar" className="post-avatar" src={profilePhoto} />
-              <p className="usr-comment-username">username</p>
+              <Avatar id="usr-comment-avatar" className="post-avatar" src={item.image} />
+              <p className="usr-comment-username">{item.name}</p>
               <span className="usr-comment-desc">
-                this is test comment. plz type right messages
+                {item.description}
               </span>
             </div>
             <div className="usr-comment-like">
@@ -89,6 +140,8 @@ const Comment = (profilePhoto, nickname, comment) => {
               </div>
             </div>
           </li>
+            );
+          })}
         </ul>
       </div>
     </div>
