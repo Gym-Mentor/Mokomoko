e638b5da38f720cd7ce9ab82179b59767b0ea198
edenist-x-x <yaron_e5@naver.com>
2021-08-19 00:09:19 +0900

    🔨 : award test

--- frontend/src/components/main/award/AwardMain.jsx
+++ frontend/src/components/main/award/AwardMain.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import AwardUser from "./AwardUser";
 import AwardTag from "./AwardTag";
 import AwardPost from "./AwardPost";
@@ -6,8 +6,30 @@ import AwardPost from "./AwardPost";
 import "../../../css/award/AwardMain.css";
 import Cheader from "../../header/Cheader";
 import { useSelector } from "react-redux";
+import axios from "axios";
 
 const AwardMain = () => {
+  const [userList, setUserList] = useState([]);
+  const [tagList, setTagList] = useState([]);
+  const [postList, setPostList] = useState([]);
+
+  useEffect(() => {
+    axios({
+      url: "https://i5d104.p.ssafy.io/api/rank",
+      method: "get",
+    })
+      .then(({ data }) => {
+        console.log("data", data);
+        console.log("data.users", data.data.users);
+        setUserList(data.data.users);
+        // setTagList(data.data)
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  console.log(userList);
   return (
     <div>
       <div className="awardMain-wrapper">
@@ -18,9 +40,9 @@ const AwardMain = () => {
             </div>
             <div className="awardMain-content">
               <br />
-              <AwardUser />
-              <AwardTag />
-              <AwardPost />
+              <AwardUser userList={userList} />
+              <AwardTag tagList={tagList} />
+              <AwardPost postList={postList} />
             </div>
           </div>
         </div>
