7b10856d803753f0ec1cd92780551b67ef0a5b2c
박종한 <jonghan1983@naver.com>
2021-08-18 18:18:05 +0900

    🔨 **develop** : 뒤로가기

--- frontend/src/components/header/ContentHeader.jsx
+++ frontend/src/components/header/ContentHeader.jsx
@@ -1,20 +1,18 @@
 import React from "react";
 import { IoIosArrowBack } from "react-icons/io";
 import "../../css/header/ContentHeader.css";
-import { useHistory, useLocation } from "react-router-dom";
 
 const ContentHeader = (props) => {
-  const location = useLocation();
   // 출력할 데이터
   const onClickBack = () => {
     console.log("뒤로 가는거 처리");
-    console.log(location.data);
     console.log(props.data);
-    if (props.data !== undefined)
-      window.history.back({
+    if (props.data !== undefined) {
+      window.history.replace({
+        pathname: `detailPresenter/${props.post.id}`,
         data: { ...props.data },
       });
-    else {
+    } else {
       window.history.back();
     }
   };
