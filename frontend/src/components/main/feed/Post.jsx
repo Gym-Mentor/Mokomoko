1b1f00251cfdd623a1a6a7fcbe490b46e55e1099
박종한 <jonghan1983@naver.com>
2021-08-18 22:57:12 +0900

    🔨 **develop** : feed Update

--- frontend/src/components/main/feed/Post.jsx
+++ frontend/src/components/main/feed/Post.jsx
@@ -3,23 +3,30 @@ import FavoriteBorderOutlinedIcon from "@material-ui/icons/FavoriteBorderOutline
 import ChatBubbleOutlinedIcon from "@material-ui/icons/ChatBubbleOutlined";
 import BookmarkBorderOutlinedIcon from "@material-ui/icons/BookmarkBorderOutlined";
 import React from "react";
-import "../../../css/main/Post.css";
+import "../../../css/main/feed/Post.css";
 import testImg from "../../../img/logo-back.png";
-
-const Post = (profilePhoto, image, username, description, timestamp) => {
+import { useSelector, useDispatch } from "react-redux";
+import { useHistory } from "react-router-dom";
+const Post = ({ contents, image, like, nickname, post }) => {
+  // 출력할 데이터
+  const dispatch = useDispatch();
+  // 출력할 데이터
+  const { user } = useSelector((state) => ({
+    user: state.userInfo.user,
+  }));
   return (
     <div className="post">
       <div className="post-content">
         <div className="post-header">
-          <Avatar className="post-avatar" src={profilePhoto} />
+          <Avatar className="post-avatar" src={image} />
           <div className="post-info">
-            <h3>{username}aaa</h3>
+            <h3>{nickname}</h3>
             {/* <p className="upload-date">{new Date(timestamp?.toDate()).toUTCString()}</p> */}
           </div>
         </div>
         <div className="post-image">
           {/* <img src={image} alt="image" /> */}
-          <img src={testImg} alt="image" />
+          <img src={contents[0].image} alt="image" />
         </div>
 
         <div className="post-things">
@@ -34,19 +41,11 @@ const Post = (profilePhoto, image, username, description, timestamp) => {
           </div>
         </div>
         <div className="likecnt">
-          <p className="feed-user-likecnt">좋아요 232,323개</p>
+          <p className="feed-user-likecnt">좋아요 {post.likeCnt}개</p>
         </div>
         <div className="post-bottom">
-          <h5 className="post-desc-username">admin </h5>
-          <p>
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum
-            has been the industry's standard dummy text ever since the 1500s, when an unknown
-            printer took a galley of type and scrambled it to make a type specimen book. It has
-            survived not only five centuries, but also the leap into electronic typesetting,
-            remaining essentially unchanged. It was popularised in the 1960s with the release of
-            Letraset sheets containing Lorem Ipsum passages, and more recently with desktop
-            publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-          </p>
+          <h5 className="post-desc-username">{nickname} </h5>
+          <p>{contents.description}</p>
         </div>
       </div>
     </div>
