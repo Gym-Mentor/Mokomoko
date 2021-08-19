48e2b52e9db63333692d31e0b7814e2c2a0ec013
Dayoung98 <iiilll_iill@naver.com>
2021-08-20 00:49:16 +0900

    🔨 **develop** :  메인피드 시간 바꾸기

--- frontend/src/components/main/feed/Post.jsx
+++ frontend/src/components/main/feed/Post.jsx
@@ -194,6 +194,49 @@ const Post = ({ contents, image, like, scrap, nickname, post, userid }) => {
     console.log(e.target);
     // setIndex(e.target.index);
   };
+
+
+  const addTime = (date) => {
+
+    //시간 변환 함수 
+
+    const splitDate = date.split("T");
+    //["2021-08-18", "12:20:40.304974"]
+
+    const splitDate2 = splitDate[1].split(":");
+    // ["12", "20", "40.304974"]
+
+    const splitDate3 = splitDate[0].split("-");
+    //[2021, 08, 18]
+    var year = splitDate3[0];
+    var month = splitDate3[1];
+    var day = splitDate3[2];
+    var hour = splitDate2[0];
+    var min = splitDate2[1];
+
+    var newDate = year+"-"+month+"-"+day+" "+hour+":"+min;
+
+    var finishDate = new Date(newDate);
+    // console.log(finishDate);
+    finishDate.setHours(finishDate.getDate()+8)
+
+    var today = finishDate;
+
+    var year = today.getFullYear();
+    var month = ('0' + (today.getMonth() + 1)).slice(-2);
+    var day = ('0' + today.getDate()).slice(-2);
+
+    var dateString = year + '-' + month  + '-' + day;
+
+    var hours = ('0' + today.getHours()).slice(-2); 
+    var minutes = ('0' + today.getMinutes()).slice(-2);
+
+
+    var dateString = year + '-' + month  + '-' + day +" "+ hours + ':' + minutes;
+
+    return dateString
+  };
+
   return (
     <div className="post">
       <div className="post-content">
@@ -201,7 +244,7 @@ const Post = ({ contents, image, like, scrap, nickname, post, userid }) => {
           <Avatar className="post-avatar" src={image} onClick={() => showUserPage(userid)} />
           <div className="post-info">
             <h3 onClick={() => showUserPage(userid)}>{nickname}</h3>
-            <p className="upload-date">{post.createdate}</p>
+            <p className="upload-date">{addTime(post.createdate)}</p>
           </div>
         </div>
         {/* <div className="post-image" onClick={showDetail}>
