851d1f80720dae7db0e665b197c98a3b57b6f03d
박종한 <jonghan1983@naver.com>
2021-08-19 21:40:48 +0900

    🔨 **develop** : 프로필 목록 추가

--- frontend/src/components/main/profile/FollowModal.jsx
+++ frontend/src/components/main/profile/FollowModal.jsx
@@ -1,8 +1,41 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "../../../css/main/profile/FollowModal.css";
 import FollowerProfile from "./FollowerProfile";
-
+import { useSelector } from "react-redux";
+import axios from "axios";
+import { setUserInfo } from "../../../modules/userInfo";
+import UserList from "./UserList";
 const FollowModal = ({ showFollowModal }) => {
+  const { user } = useSelector((state) => ({
+    user: state.userInfo.user,
+  }));
+  const { OtherUser } = useSelector((state) => state.OtherUser);
+
+  const [userList, setUserList] = useState([]);
+  // 팔로워 목록 받기
+  useEffect(() => {
+    axios({
+      url: "https://i5d104.p.ssafy.io/api/user/follow/",
+      method: "get",
+      data: {
+        selectid: OtherUser.user.id,
+        userid: user.id,
+      },
+    })
+      .then(({ data }) => {
+        console.log(data);
+        let newList = Object.assign([], userList);
+        newList = data.data;
+        setUserList(newList);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  useEffect(() => {
+    return () => {};
+  }, [userList]);
   return (
     <>
       <div className="background" onClick={showFollowModal} />
@@ -11,18 +44,8 @@ const FollowModal = ({ showFollowModal }) => {
           <p>팔로우</p>
         </div>
         <hr className="follower-hr" />
-        <FollowerProfile />
-        <FollowerProfile />
-        <FollowerProfile />
-        <FollowerProfile />
-        <FollowerProfile />
-        <FollowerProfile />
-        <FollowerProfile />
-        <FollowerProfile />
-        <FollowerProfile />
-        <FollowerProfile />
-        <FollowerProfile />
-        <FollowerProfile />
+
+        <UserList list={userList} />
       </div>
     </>
   );
